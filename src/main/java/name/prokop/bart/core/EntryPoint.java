a0ab0a6e6045ce5e3e5c10edf4b7b0ffa58c9f94
Bart Prokop <prokop.bart@gmail.com>
2017-09-21 02:16:06 +0100

    Adding reactor

--- src/main/java/name/prokop/bart/core/EntryPoint.java
+++ src/main/java/name/prokop/bart/core/EntryPoint.java
@@ -4,6 +4,7 @@ import io.undertow.Undertow;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+import reactor.core.publisher.Flux;
 
 /**
  *
@@ -12,16 +13,27 @@ import io.undertow.util.Headers;
 public class EntryPoint {
 
     public static void main(String... args) {
-        Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(new HttpHandler() {
-                    @Override
-                    public void handleRequest(final HttpServerExchange exchange) throws Exception {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
+        Flux<HttpServerExchange> requestPublisher = Flux.create(emitter -> {
+            Undertow.Builder undertowBuilder = Undertow.builder();
+            undertowBuilder.addHttpListener(8080, "localhost");
+            undertowBuilder.setHandler(new HttpHandler() {
+                @Override
+                public void handleRequest(final HttpServerExchange exchange) throws Exception {
+                    if (exchange.isInIoThread()) {
+                        exchange.dispatch(this);
+                        return;
                     }
-                }).build();
-        server.start();
+                    emitter.next(exchange);
+                }
+            });
+            Undertow server = undertowBuilder.build();
+            server.start();
+        });
+
+        requestPublisher.subscribe(exchange -> {
+            exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+            exchange.getResponseSender().send("Hello World");
+        });
     }
 
 }
